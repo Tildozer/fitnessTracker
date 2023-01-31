4ce805e7763f888a117fbd3c69f4b76ec636b27b
erictorres0123 <erictorres0123@gmail.com>
2023-01-31 17:09:19 -0600

    hashed passowrd

--- db/users.js
+++ db/users.js
@@ -1,16 +1,22 @@
 const client = require("./client");
+const bcrypt = require("bcrypt")
+
+const saltRounds = 10;
 
 // database functions
 
 // user functions
 async function createUser({ username, password }) {
   try {
+    const hashedPassword = await bcrypt.hash(password, saltRounds)  
+    console.log(hashedPassword);
+
     const SQL = `
     INSERT INTO users(username, password)
     VALUES ($1, $2)
     RETURNING id, username
     `
-    const { rows: [user] } = await client.query(SQL, [username, password]) 
+    const { rows: [user] } = await client.query(SQL, [username, hashedPassword]) 
     // console.log('user', user)
     return user;
 
@@ -22,17 +28,23 @@ async function createUser({ username, password }) {
 async function getUser({ username, password }) {
 try {
   const SQL = `
-  SELECT id, username FROM users 
+  SELECT * 
+  FROM users 
   WHERE username =$1
-  AND password =$2
   `
-const { rows: [user] } = await client.query(SQL, [username, password])
+const { rows: [user] } = await client.query(SQL, [username])
 // console.log(user);
 if (!user) {
   throw new Error('Could not get user');
 }
-  
-  return user;
+  const hashedPassword = user.password;
+  const match = await bcrypt.compare(password, hashedPassword)
+
+  if (match) {
+    delete user.password;
+    return user;
+  }  
+
 } catch (error) {
   console.error(error);
 }
