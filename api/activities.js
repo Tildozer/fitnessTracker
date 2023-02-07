e00acd52c4d0e1332d2e08c48358a5617250fcf9
erictorres0123 <erictorres0123@gmail.com>
2023-02-07 08:54:18 -0600

    get activities done, started activitiesbydId

--- api/activities.js
+++ api/activities.js
@@ -1,9 +1,45 @@
 const express = require('express');
+const { getAllActivities, getPublicRoutinesByActivity } = require('../db');
 const router = express.Router();
 
 // GET /api/activities/:activityId/routines
 
+router.get ('/:activityId/routines', async (res, req, next) => {
+const { activityId } = req.params;
+
+try {
+    const routinesByActivity = await getPublicRoutinesByActivity({id: activityId, });
+
+if (!routinesByActivity.length) {
+    next({
+        error: '',
+        message: `Activity ${activityId} not found`,
+        name: 'Activity Not Found'
+    });
+} else {
+    res.send(routinesByActivity);
+}
+
+} catch (error) {
+    next(error);
+}
+
+});
+
 // GET /api/activities
+
+router.get ('/', async (req, res, next) => {
+    
+    try {
+
+        const getActivities = await getAllActivities()
+
+        res.send(getActivities);
+    } catch (error) {
+       next(error); 
+    }
+
+})
 
 // POST /api/activities
 
